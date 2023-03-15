a9f3f8796f2df9908c19bb00cc2368095ee9cb22
SlimCandy <cgpnrjmhppeb@ro.ru>
2023-03-15 17:31:07 +0100

    Pull IndexPage from Sanity

---
+++ gatsby-node.ts
@@ -0,0 +1,54 @@
+import { type CreatePagesArgs, type GatsbyNode } from 'gatsby'
+import path from 'path'
+
+async function turnArticlesIntoPages({
+  graphql,
+  actions
+}: CreatePagesArgs): Promise<void> {
+  // 1. Get a template for this page
+  const postTemplate = path.resolve('./src/pages/single-post.tsx')
+  // 2. Query all posts
+
+  const { data } = await graphql<Queries.AllPostsQuery>(`
+    query AllPosts {
+      allSanityPost(sort: { publishedAt: DESC }, limit: 100) {
+        nodes {
+          title
+          slug {
+            current
+          }
+        }
+      }
+    }
+  `)
+
+  const posts =
+    data?.allSanityPost.nodes !== undefined &&
+    data.allSanityPost.nodes !== null &&
+    data.allSanityPost.nodes.length > 0
+      ? data.allSanityPost.nodes
+      : []
+  // 3. Loop over each post and create a page for that post
+  posts.forEach(post => {
+    console.log('Creating page for Article:', post.title)
+
+    const link =
+      post.slug?.current !== undefined && post.slug.current !== null
+        ? post.slug.current
+        : ''
+    if (link.length > 0)
+      actions.createPage({
+        path: `posts/${link}`,
+        component: postTemplate,
+        context: {
+          slug: link
+        }
+      })
+  })
+}
+
+export const createPages: GatsbyNode['createPages'] =
+  async function createPages(params) {
+    // Create pages dynamically
+    await turnArticlesIntoPages(params)
+  }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,195 +1,113 @@
 import * as React from 'react'
-import type { HeadFC, PageProps } from 'gatsby'
+import { graphql, type HeadProps, Link, type PageProps } from 'gatsby'
 
-const pageStyles = {
-  color: '#232129',
-  padding: 96,
-  fontFamily: '-apple-system, Roboto, sans-serif, serif'
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320
-}
-const headingAccentStyles = {
-  color: '#663399'
-}
-const paragraphStyles = {
-  marginBottom: 48
-}
-const codeStyles = {
-  color: '#8A6534',
-  padding: 4,
-  backgroundColor: '#FFF4DB',
-  fontSize: '1.25rem',
-  borderRadius: 4
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0
-}
-const doclistStyles = {
-  paddingLeft: 0
-}
-const listItemStyles = {
-  fontWeight: 300,
-  fontSize: 24,
-  maxWidth: 560,
-  marginBottom: 30
-}
-
-const linkStyle = {
-  color: '#8954A8',
-  fontWeight: 'bold',
-  fontSize: 16,
-  verticalAlign: '5%'
-}
-
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: 'none',
-  display: `inline-block`,
-  marginBottom: 24,
-  marginRight: 12
-}
-
-const descriptionStyle = {
-  color: '#232129',
-  fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25
-}
-
-const docLinks = [
-  {
-    text: 'TypeScript Documentation',
-    url: 'https://www.gatsbyjs.com/docs/how-to/custom-configuration/typescript/',
-    color: '#8954A8'
-  },
-  {
-    text: 'GraphQL Typegen Documentation',
-    url: 'https://www.gatsbyjs.com/docs/how-to/local-development/graphql-typegen/',
-    color: '#8954A8'
-  }
-]
-
-const badgeStyle = {
-  color: '#fff',
-  backgroundColor: '#088413',
-  border: '1px solid #088413',
-  fontSize: 11,
-  fontWeight: 'bold',
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: '4px 6px',
-  display: 'inline-block',
-  position: 'relative' as 'relative',
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1
-}
-
-const links = [
-  {
-    text: 'Tutorial',
-    url: 'https://www.gatsbyjs.com/docs/tutorial/',
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: '#E95800'
-  },
-  {
-    text: 'How to Guides',
-    url: 'https://www.gatsbyjs.com/docs/how-to/',
-    description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: '#1099A8'
-  },
-  {
-    text: 'Reference Guides',
-    url: 'https://www.gatsbyjs.com/docs/reference/',
-    description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: '#BC027F'
-  },
-  {
-    text: 'Conceptual Guides',
-    url: 'https://www.gatsbyjs.com/docs/conceptual/',
-    description:
-      'Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.',
-    color: '#0D96F2'
-  },
-  {
-    text: 'Plugin Library',
-    url: 'https://www.gatsbyjs.com/plugins',
-    description:
-      'Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.',
-    color: '#8EB814'
-  },
-  {
-    text: 'Build and Host',
-    url: 'https://www.gatsbyjs.com/cloud',
-    badge: true,
-    description:
-      'Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!',
-    color: '#663399'
-  }
-]
-
-const IndexPage: React.FC<PageProps> = () => {
+function NoPosts(): React.ReactElement {
   return (
-    <main style={pageStyles}>
-      <h1 style={headingStyles}>
-        Congratulations
-        <br />
-        <span style={headingAccentStyles}>
-          — you just made a Gatsby site! 🎉🎉🎉
-        </span>
-      </h1>
-      <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.tsx</code> to see this
-        page update in real-time. 😎
-      </p>
-      <ul style={doclistStyles}>
-        {docLinks.map(doc => (
-          <li key={doc.url} style={docLinkStyle}>
-            <a
-              style={linkStyle}
-              href={`${doc.url}?utm_source=starter&utm_medium=ts-docs&utm_campaign=minimal-starter-ts`}
-            >
-              {doc.text}
-            </a>
-          </li>
-        ))}
-      </ul>
-      <ul style={listStyles}>
-        {links.map(link => (
-          <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
-            <span>
-              <a
-                style={linkStyle}
-                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter-ts`}
-              >
-                {link.text}
-              </a>
-              {link.badge === true && (
-                <span style={badgeStyle} aria-label="New Badge">
-                  NEW!
-                </span>
-              )}
-              <p style={descriptionStyle}>{link.description}</p>
-            </span>
-          </li>
-        ))}
-      </ul>
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-      />
-    </main>
+    <p>
+      No posts found. Please create a post in Sanity Studio and publish it to
+      see it here.
+    </p>
+  )
+}
+
+function IndexPage({
+  data: {
+    allSanityPost: { nodes },
+    site
+  }
+}: PageProps<Queries.IndexPageQuery>): React.ReactElement {
+  const posts = nodes !== null && nodes.length > 0 ? nodes : []
+  if (posts.length === 0) {
+    return <NoPosts />
+  }
+
+  const featuredPost = {
+    title: posts[0].title,
+    description: posts[0].description,
+    link:
+      posts[0].slug?.current !== undefined && posts[0].slug.current !== null
+        ? `/posts/${posts[0].slug.current}`
+        : '',
+    readMore:
+      posts[0].readMore === null ? 'Continue reading...' : posts[0].readMore
+  }
+  const otherPosts = posts.slice(1).map(post => ({
+    title: post.title,
+    description: post.description,
+    link:
+      post.slug?.current !== undefined && post.slug.current !== null
+        ? `/posts/${post.slug.current}`
+        : '',
+    readMore: post.readMore === null ? 'Continue reading...' : post.readMore
+  }))
+  const siteMetadata = site?.siteMetadata
+
+  return (
+    <>
+      <header>
+        <h1>{siteMetadata?.title}</h1>
+        <p>{siteMetadata?.description}</p>
+      </header>
+      <hr />
+      <main>
+        <article>
+          <h2>{featuredPost.title}</h2>
+          <p>{featuredPost.description}</p>
+          {featuredPost.link.length > 0 && (
+            <p>
+              <Link to={featuredPost.link}>{featuredPost.readMore}</Link>
+            </p>
+          )}
+        </article>
+        {otherPosts.length > 0 && (
+          <ul>
+            {otherPosts.map(post => (
+              <li key={post.title}>
+                <article>
+                  <h3>{post.title}</h3>
+                  <p>{post.description}</p>
+                  {post.link.length > 0 && (
+                    <p>
+                      <Link to={post.link}>{post.readMore}</Link>
+                    </p>
+                  )}
+                </article>
+              </li>
+            ))}
+          </ul>
+        )}
+      </main>
+    </>
   )
 }
 
 export default IndexPage
+export function Head({
+  data: { site }
+}: HeadProps<Queries.IndexPageQuery>): React.ReactElement {
+  const siteMetadata = site?.siteMetadata
 
-export const Head: HeadFC = () => <title>Home Page</title>
+  return <title>{siteMetadata?.title}</title>
+}
+
+export const query = graphql`
+  query IndexPage {
+    allSanityPost {
+      nodes {
+        title
+        readMore
+        slug {
+          current
+        }
+        description
+        publishedAt(formatString: "DD MMMM YYYY")
+      }
+    }
+    site {
+      siteMetadata {
+        title
+        description
+      }
+    }
+  }
+`
